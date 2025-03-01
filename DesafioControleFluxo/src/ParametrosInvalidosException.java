c127e9202fac6746a9ab6ecbccc728afbe79ec69
Lucca Oliveira <92926937+Luccado@users.noreply.github.com>
2025-03-01 13:07:27 -0400

    Desafio

---
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,5 @@
+public class ParametrosInvalidosException extends Throwable {
+    public ParametrosInvalidosException() {
+        super("o Segundo parâmreto deve ser maior que o primeiro");
+    }
+}
